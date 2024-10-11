d73d1f4493e170def37862d256bb4fee3cfa6c7e
shinespark <shnsprk@gmail.com>
2024-10-12 00:16:41 +0900

    6.3 アプリ起動時のスタート画面を作成する

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:edit_snap/start_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const StartScreen(),
     );
   }
 }
