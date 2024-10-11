c093c5ca9c824856d38244802d427b064f98c29e
shinespark <shnsprk@gmail.com>
2024-10-12 00:35:12 +0900

    6.5 アプリを日本語化する

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:edit_snap/start_screen.dart';
 
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: L10n.localizationsDelegates,
+      supportedLocales: L10n.supportedLocales,
       title: 'Edit snap',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = L10n.of(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Edit snap'),
+        title: Text(l10n.startScreenTitle),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'こんにちは！\n今日は${DateTime.now()}です。',
+              l10n.helloWorldOn(DateTime.now()),
               textAlign: TextAlign.center,
             ),
             ElevatedButton(
-              child: const Text('開始する'),
+              child: Text(l10n.start),
               onPressed: () {},
             )
           ],
