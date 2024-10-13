e18dc99dadaac00460c9efc7108b4bf17820a03e
shinespark <shnsprk@gmail.com>
2024-10-14 01:25:29 +0900

    画像ライブラリから画像を取得する

--- lib/image_select_screen.dart
+++ lib/image_select_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as image_lib;
 
 class ImageSelectScreen extends StatefulWidget {
   const ImageSelectScreen({super.key});
@@ -9,9 +12,46 @@ class ImageSelectScreen extends StatefulWidget {
 }
 
 class _ImageSelectScreenState extends State<ImageSelectScreen> {
+  // 画像ライブラリやカメラにアクセスする機能を持つ
+  final ImagePicker _picker = ImagePicker();
+  // 8bit 符号なし整数のリスト
+  Uint8List? _imageBitmap;
+
+  Future<void> _selectImage() async {
+    // 画像を選択する
+    final XFile? imageFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+
+    // ファイルオブジェクトから画像データを取得する
+    final imageBitmap = await imageFile?.readAsBytes();
+    assert(imageBitmap != null);
+    if (imageBitmap == null) return;
+
+    // 画像データをデコードする
+    final image = image_lib.decodeImage(imageBitmap);
+    assert(image != null);
+    if (image == null) return;
+
+    // 画像データとメタデータを内包したクラス
+    final image_lib.Image resizedImage;
+    if (image.width > image.height) {
+      // 横長なら500pxにリサイズ
+      resizedImage = image_lib.copyResize(image, width: 500);
+    } else {
+      // 縦長なら500pxにリサイズ
+      resizedImage = image_lib.copyResize(image, height: 500);
+    }
+
+    // 画像をエンコードいて状態を更新する
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(resizedImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
+    final imageBitmap = _imageBitmap;
 
     return Scaffold(
       appBar: AppBar(
@@ -21,14 +61,16 @@ class _ImageSelectScreenState extends State<ImageSelectScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            if (imageBitmap != null) Image.memory(imageBitmap),
             ElevatedButton(
               child: Text(l10n.imageSelect),
-              onPressed: () {},
+              onPressed: () => _selectImage(),
             ),
-            ElevatedButton(
-              child: Text(l10n.imageEdit),
-              onPressed: () {},
-            )
+            if (imageBitmap != null)
+              ElevatedButton(
+                child: Text(l10n.imageEdit),
+                onPressed: () {},
+              )
           ],
         ),
       ),
