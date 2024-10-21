70a9635ae9fbeaacac3ea98356de7fe9318e92a4
shinespark <shnsprk@gmail.com>
2024-10-22 01:23:22 +0900

    画像を編集する処理を実装する

--- lib/edit_snap_screen.dart
+++ lib/edit_snap_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image/image.dart' as image_lib;
 
 import 'package:edit_snap/gen/assets.gen.dart';
 
@@ -14,6 +15,36 @@ class ImageEditScreen extends StatefulWidget {
 }
 
 class _ImageEditScreenState extends State<ImageEditScreen> {
+  late Uint8List _imageBitmap;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageBitmap = widget.imageBitmap;
+  }
+
+  void _rotateImage() {
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image == null) return;
+    final rotatedImage = image_lib.copyRotate(image, angle: 90);
+
+    // 状態を更新
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(rotatedImage);
+    });
+  }
+
+  void _flipImage() {
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image == null) return;
+    final flippedImage = image_lib.flipHorizontal(image);
+
+    // 状態を更新
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(flippedImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
@@ -26,16 +57,16 @@ class _ImageEditScreenState extends State<ImageEditScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.memory(widget.imageBitmap),
+            Image.memory(_imageBitmap),
             IconButton(
-              onPressed: () {},
+              onPressed: () => _rotateImage(),
               icon: Assets.rotateIcon.svg(
                 width: 24,
                 height: 24,
               ),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => _flipImage(),
               icon: Assets.flipIcon.svg(
                 width: 24,
                 height: 24,
